808e45a710f3d122aa48cfb6292bfdfb472e4118
Dawid <dawid.marchewczyk@gmail.com>
2024-06-19 18:31:46 +0200

    create a render function

---
+++ js/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+  {
+    id: 2,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+  {
+    id: 3,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+  {
+    id: 4,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+  {
+    id: 5,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+  {
+    id: 6,
+    img: "/img/pexels-mallonymedia-5413302.jpg",
+    productTitle: "Sneakers Nike Jordan One",
+    productPrice: 149.99,
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+import { products } from "./products.js";
+
 // REVIEWS CAROUSEL
+const favouritesProducts = [];
+const productsList = document.querySelector(".products");
 const controls = document.querySelector(".controls");
 const reviewsContainer = document.querySelector(".reviews");
 
@@ -17,3 +21,35 @@ controlsButtons.forEach((controlBtn, index) => {
     controlBtn.classList.add("controls__review--active");
   });
 });
+
+const renderProducts = (data) => {
+  data.forEach((item) => {
+    const productEl = document.createElement("div");
+    productEl.classList.add("product");
+    productEl.id = item.id;
+    productEl.innerHTML = `
+<div class="product__img">
+  <button class="add-to-favourite">
+    <i class="fa-regular fa-heart product__liked"></i>
+  </button>
+  <img
+    src=${item.img}
+    alt="shoes"
+    loading="lazy"
+  />
+</div>
+<div class="product__content">
+  <div class="product__description">
+    <h3 class="product__title">${item.productTitle}</h3>
+    <p class="product__price">$ ${item.productPrice}</p>
+  </div>
+  <button class="add-to-cart btn">Add to cart</button>
+</div>
+`;
+    productsList.insertAdjacentElement("afterbegin", productEl);
+  });
+};
+
+renderProducts(products);
+
+const addToFavourites = (id) => {};
